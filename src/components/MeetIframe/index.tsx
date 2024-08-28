bcacd148b61e636edab69b46d97af8a3283ff007
MbeHenri <henri.mbe@facsciences-uy1.cm>
2024-08-28 15:41:45 +0100

    update meetiframe

--- src/components/MeetIframe/index.tsx
+++ src/components/MeetIframe/index.tsx
@@ -16,57 +16,54 @@ export const MeetIframe: React.FC<MeetIframeProps> = ({
 
   const [showPreloader, setShowPreloader] = useState(false);
 
-  const handleLoad = useCallback(() => {
-    try {
-      const iframe = iframeRef.current;
-      const iframeWindow = iframe.contentWindow;
-      if (!iframeWindow) {
-        /* console.error("Impossible d'accéder à la fenêtre de l'iframe."); */
-        return;
-      }
-
-      const iframeDocument = iframeWindow.document;
-
-      // Remplacer les sélecteurs par les IDs ou classes réels utilisés par Nextcloud
-      const userInput = iframeDocument.querySelector(
-        'input[name="user"]'
-      ) as HTMLInputElement | null;
-      const passwordInput = iframeDocument.querySelector(
-        'input[name="password"]'
-      ) as HTMLInputElement | null;
-      const loginForm = iframeDocument.querySelector(
-        "form"
-      ) as HTMLFormElement | null;
-
-      if (userInput && passwordInput && loginForm) {
-        userInput.value = username;
-        passwordInput.value = token;
-
-        // Soumettre le formulaire
-        loginForm.submit();
-
-        /* console.log("Formulaire de connexion soumis."); */
-      } else {
-        console.error(
-          "Champs de formulaire introuvables dans l'iframe chargeant l'url " +
-            url
-        );
-      }
-    } catch (error) {
-      console.error(
-        "Erreur lors de l'accès au contenu de l'iframe chargeant l'url " +
-          url +
-          " :",
-        error
-      );
-    }
-    setShowPreloader(false);
-  }, [token, username, url]);
-
   useEffect(() => {
     const iframe = iframeRef.current;
     if (iframe) {
       setShowPreloader(true);
+      const handleLoad = () => {
+        try {
+          const iframeWindow = iframe.contentWindow;
+          if (!iframeWindow) {
+            console.error("Impossible d'accéder à la fenêtre de l'iframe.");
+            return;
+          }
+
+          const iframeDocument = iframeWindow.document;
+
+          const userInput = iframeDocument.querySelector(
+            'input[name="user"]'
+          ) as HTMLInputElement | null;
+          const passwordInput = iframeDocument.querySelector(
+            'input[name="password"]'
+          ) as HTMLInputElement | null;
+          const loginForm = iframeDocument.querySelector(
+            "form"
+          ) as HTMLFormElement | null;
+
+          if (userInput && passwordInput && loginForm) {
+            userInput.value = username;
+            passwordInput.value = token;
+
+            // Soumettre le formulaire
+            loginForm.submit();
+            // on affiche le preloader
+
+            setShowPreloader(true);
+            // eslint-disable-next-line no-console
+            console.log("Formulaire de connexion soumis.");
+            //console.log('username', userInput.value, 'password', passwordInput.value);
+          } else {
+            setShowPreloader(false);
+          }
+        } catch (error) {
+          console.error(
+            "Erreur lors de l'accès au contenu de l'iframe :",
+            error
+          );
+          setShowPreloader(false);
+        }
+      };
+
       // Associer l'événement de chargement
       iframe.addEventListener("load", handleLoad);
 
@@ -75,7 +72,7 @@ export const MeetIframe: React.FC<MeetIframeProps> = ({
         iframe.removeEventListener("load", handleLoad);
       };
     }
-  }, [handleLoad]);
+  }, [token, username]);
 
   return (
     <div className={styles.contentViewWrapper}>
